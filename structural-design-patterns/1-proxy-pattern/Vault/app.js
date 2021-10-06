8c97fcdd927b5fb740836815dbc8f50a0cd2aa56
chingsley <eneja.kc@gmail.com>
2021-10-07 00:33:01 +0100

    - Practice the proxy pattern

---
+++ structural-design-patterns/1-proxy-pattern/Vault/app.js
@@ -0,0 +1,19 @@
+const { lock, unlock } = require('./Vault');
+const password = 'thisisastrongpassword123';
+
+const obj = {
+  name: 'John Doe',
+  age: 32,
+  city: 'Chicago',
+  country: 'US',
+};
+
+const person = lock(obj, password);
+console.log(person);
+person.favColor = 'Red';
+person.favFruit = 'Apple';
+console.log(person);
+console.log(person.name);
+
+const unlocked = unlock(person, password);
+console.log(unlocked);
