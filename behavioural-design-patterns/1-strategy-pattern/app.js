8991d5a9fb0717c4ddbab5b8ae54c2cee8f2124c
chingsley <eneja.kc@gmail.com>
2022-03-03 17:45:08 +0100

    - Practice the strategy design pattern

---
+++ behavioural-design-patterns/1-strategy-pattern/app.js
@@ -0,0 +1,9 @@
+const Payment = require('./Payment');
+const Chasebank = require('./Chasebank');
+const Citibank = require('./Citibanks');
+
+const payment = new Payment();
+
+payment.pay(new Chasebank(200, '0002340112450'));
+payment.pay(new Citibank(234, '0101901889181'));
+payment.refund(new Chasebank(78, '00998199181'));
