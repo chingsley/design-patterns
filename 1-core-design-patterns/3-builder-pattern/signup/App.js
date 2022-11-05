ffeaa15df2758dd24d52a04b3c09316d0c3308cd
Kingsley Eneja <kingsley.eneja@getir.com>
2022-11-05 22:53:07 +0100

    - Update folder structure

--- 1-core-design-patterns/3-builder-pattern/signup/App.js
+++ 1-core-design-patterns/3-builder-pattern/signup/App.js
@@ -2,7 +2,7 @@ const SignupBuilder = require("./SignupBuilder");
 
 new SignupBuilder("John Doe", "john@gmail.com", 25)
   .setPhoto("johndoe.jpg")
-  .setNick("john_D")
+  .setNickname("john_D")
   .setTopics(["Node.js", "Javascript"])
   .setSME(["Node.js"])
   .setModerator()
